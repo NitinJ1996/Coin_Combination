5952ae70b31622590975d29175956124abc9b67e
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 23:18:14 +0530

    {Nitin} UseCase 2 - Percentage calculation for singlet Update

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,7 +1,34 @@
 #!/bin/bash -x
 
-coinflip=$((RANDOM%2))
-case $coinflip in
-		1) echo "HEADS" ;;
-		*) echo "TAILS" ;;
-esac
+#constants
+heads=1
+tails=0
+
+read -p "Enter the no.of time to flip a coin: " num
+
+#variables
+head_count=0
+tail_count=0
+
+
+declare -A singlet
+
+for ((i=0; i<$num; i++))
+do
+	coinflip=$((RANDOM%2))
+	case $coinflip in
+			$heads) singlet["H"]=$((++head_count)) ;;
+			$tails) singlet["T"]=$((++tail_count)) ;;
+			*) ;;
+	esac
+done
+
+declare -A singlet_percent
+for key in ${!singlet[@]}
+do
+		singlet_percent[$key]=`echo "scale=2; $((${singlet[$key]}))/$num*100" | bc`
+done
+
+echo "Signlet Combination: " ${!singlet[@]}
+echo "Signlet Count: " ${singlet[@]}
+echo "Singlet Percentage: "${singlet_percent[@]}
